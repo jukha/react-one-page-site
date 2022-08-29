4849e7956f41b5e8ddcd78599d2479e453623427
m_junaid_07 <junaid@xenex-media.com.au>
2022-08-29 20:16:55 +0500

    change modal styling

--- src/Home/index.js
+++ src/Home/index.js
@@ -453,14 +453,16 @@ const Home = () => {
         aria-labelledby="exampleModalLabel1"
         aria-hidden="true"
       >
-        <div className="modal-dialog modal-lg">
+        <div className="modal-dialog modal-lg modal-dialog-centered ">
           <div className="modal-content">
+            <div className="modal-header">
             <button
               type="button"
               className="btn-close"
               data-bs-dismiss="modal"
               aria-label="Close"
             ></button>
+            </div>
             <div className="modal-body">
               <iframe
                 width="916"
@@ -483,14 +485,16 @@ const Home = () => {
         aria-labelledby="exampleModalLabel2"
         aria-hidden="true"
       >
-        <div className="modal-dialog">
+        <div className="modal-dialog modal-lg modal-dialog-centered ">
           <div className="modal-content">
+            <div className="modal-header">
             <button
               type="button"
               className="btn-close"
               data-bs-dismiss="modal"
               aria-label="Close"
             ></button>
+            </div>
             <div className="modal-body">
               <iframe
                 width="916"
